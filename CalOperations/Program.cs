86702f518453377d80580671465c6e3cec8f39a9
Arunkumar K <Arunkumar@JKTECH>
2019-12-16 11:30:33 +0530

    Division Operation was added

--- CalOperations/Program.cs
+++ CalOperations/Program.cs
@@ -16,7 +16,7 @@ namespace CalOperations
             int n1=  Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter another Number:");
             int n2= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter your Math Operation: \n Press 1 to Addition \n Press 2 to substract\n Press 3 to Multiplication\n");
+            Console.WriteLine("Enter your Math Operation: \n Press 1 to Addition \n Press 2 to substract\n Press 3 to Multiplication\n Press 4 to Division\n");
             int n3= Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your Option is : "+n3);
             log.Info("You have entered numbers are "+n1+" and "+n2);
@@ -36,9 +36,15 @@ namespace CalOperations
                break;
                case 3:
                 log.Info("Your Option is Multiplication");
-                int result2=_MathOperations.Multiplication(n1,n2);
-                Console.WriteLine("Multiplication of "+n1+" and "+n2+" is "+result2);
-                log.Info("Multiplication of "+n1+" and "+n2+" is "+result2);
+                int result=_MathOperations.Multiplication(n1,n2);
+                Console.WriteLine("Multiplication of "+n1+" and "+n2+" is "+result);
+                log.Info("Multiplication of "+n1+" and "+n2+" is "+result);
+               break;
+               case 4:
+                log.Info("Your Option is Division");
+                int result2=_MathOperations.Division(n1,n2);
+                Console.WriteLine("Division of "+n1+" and "+n2+" is "+result2);
+                log.Info("Division of "+n1+" and "+n2+" is "+result2);
                break;
                default:
                 Console.WriteLine("Invalid option!!!");
--- MathOperationsLib/IMathOperations.cs
+++ MathOperationsLib/IMathOperations.cs
@@ -7,5 +7,6 @@ namespace MathOperationsLib
          int Addition(int a, int b);
          int Substraction(int a, int b);
          int Multiplication(int a, int b);
+         int Division(int a, int b);
     } 
 }
